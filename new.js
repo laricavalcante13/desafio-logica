a0d26c110cf5a7bf9a65feaa79f15f8f8b94fe07
laricavalcante13 <lari.cavalcante13@gmail.com>
2023-11-05 00:47:25 -0300

    comecei

---
+++ new.js
@@ -0,0 +1,8 @@
+let nome = "Pikachu";
+let xp = 200;
+
+if(xp <= 1000) {
+  console.log ("O heroi " + nome + " está no nível ferro.")
+  
+} else if(xp > 1000 && xp <= 2000);
+  console.log ("O heroi " + nome + " está no nível bronze.")
